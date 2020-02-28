d1fb9bc3fcfc18b65fdb1da63c9f7356dcb6474a
Nate <nate.rosenstock@hakai.org>
2020-02-27 17:29:16 -0800

    change gsed to sed

--- meds/meds_download.sh
+++ meds/meds_download.sh
@@ -24,10 +24,10 @@ unzip -LL -u "$zip_folder/*_csv.zip" -d $csv_folder
 echo "Fixing up CSV headers.."
 
 # Remove trailing comma from first line of each file
-gsed -i "1,1s/,\W*$//" $csv_folder/*.csv
+sed -i "1,1s/,\W*$//" $csv_folder/*.csv
 
 # Columns ATMS,GSPD,WSPD,WDIR are duplicated so I added '2' 
 # set this as the new header
-gsed -i "1c\STN_ID,DATE,Q_FLAG,LATITUDE,LONGITUDE,DEPTH,VCAR,VTPK,VWH\$,VCMX,VTP\$,WDIR,WSPD,WSS\$,GSPD,WDIR2,WSPD2,WSS\$2,GSPD2,ATMS,ATMS2,DRYT,SSTP" $csv_folder/c*.csv
+sed -i "1c\STN_ID,DATE,Q_FLAG,LATITUDE,LONGITUDE,DEPTH,VCAR,VTPK,VWH\$,VCMX,VTP\$,WDIR,WSPD,WSS\$,GSPD,WDIR2,WSPD2,WSS\$2,GSPD2,ATMS,ATMS2,DRYT,SSTP" $csv_folder/c*.csv
 
 echo "Done"
