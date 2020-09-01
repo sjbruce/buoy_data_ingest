b8e450dab55f3d09ffbdef0ab2ecc3270603c65e
CrazyJoeMalloy <grimmy.the.grim.reaper@gmail.com>
2020-09-01 17:48:39 -0230

    Working model of ingesting SWOB and converting to CSV
     - buoy_list.txt has been updated to include atlantic stations instead of pacific
     - ingest_to_csv.conf contains configuration for csv plugin, including location of buoy_list.txt, output director, column headers datetime_format (for output files) and output file name format (assuming station id is the first value and output datetime is the second)
     - reworked basic structure to use pandas dataframes and existing CSV files as sources instead of using a database connection, largely driven by configuration.
    - todo: add argument to supply configuration file, currently hard coded, work on examples using multiple sample files that cross daily boundaries

--- msc-datamart/msc_ingest/ingest_to_csv.py
+++ msc-datamart/msc_ingest/ingest_to_csv.py
@@ -1,196 +1,55 @@
 '''
-
-Script that is run by a sarracenia plugin when new buoy data is received, inserts parsed XML into postgres database table `swob_marine`
-
+Script that is run by a sarracenia plugin when new buoy data is received, 
+inserts parsed XML into flat files for ingestion by ERDDAP
 
 See docs at https://dd4.weather.gc.ca/observations/doc/
-
 '''
 
-# TODO add logging
 # TODO could auto-create table here
 
 import os
 import click
 import pandas as pd
+from datetime import datetime
 from msc_ingest.parse_xml import buoy_xml_to_json
 
-# TODO: Move these values/definitions to an external configuration file
-buoy_filename = "buoy_list.txt"
-output_dir = 'H:/Temp/msc-datamart/data/'
-column_headers = [
-    'sampling_time',
-    'result_time',
-    'stn_typ',
-    'wmo_synop_id',
-    'wmo_identifier_extended',
-    'stn_nam',
-    'msc_id',
-    'stn_elev',
-    'sensor_table_number',
-    'lat',
-    'long',
-    'date_tm',
-    'buoy_typ',
-    'rpt_typ',
-    'crnt_buoy_lat',
-    'crnt_buoy_lat_qa_summary',
-    'crnt_buoy_lat_data_flag',
-    'crnt_buoy_long',
-    'crnt_buoy_long_qa_summary',
-    'crnt_buoy_long_data_flag',
-    'avg_crnt_volt_pst10mts',
-    'avg_crnt_volt_pst10mts_1',
-    'avg_solr_panl_crnt_pst10mts',
-    'avg_solr_panl_crnt_pst10mts_qa_summary',
-    'avg_solr_panl_crnt_pst10mts_data_flag',
-    'avg_solr_panl_crnt_pst10mts_1',
-    'avg_solr_panl_crnt_pst10mts_1_qa_summary',
-    'avg_solr_panl_crnt_pst10mts_1_data_flag',
-    'avg_batry_volt_pst10mts',
-    'avg_batry_volt_pst10mts_qa_summary',
-    'avg_batry_volt_pst10mts_data_flag',
-    'avg_batry_volt_pst10mts_1',
-    'avg_batry_volt_pst10mts_1_qa_summary',
-    'avg_batry_volt_pst10mts_1_data_flag',
-    'avg_air_temp_pst10mts',
-    'avg_air_temp_pst10mts_qa_summary',
-    'avg_air_temp_pst10mts_data_flag',
-    'avg_air_temp_pst10mts_1',
-    'avg_air_temp_pst10mts_1_qa_summary',
-    'avg_air_temp_pst10mts_1_data_flag',
-    'avg_stn_pres_pst10mts',
-    'avg_stn_pres_pst10mts_qa_summary',
-    'avg_stn_pres_pst10mts_data_flag',
-    'avg_stn_pres_pst10mts_1',
-    'avg_stn_pres_pst10mts_1_qa_summary',
-    'avg_stn_pres_pst10mts_1_data_flag',
-    'avg_stn_pres_pst10mts_2',
-    'avg_stn_pres_pst10mts_2_qa_summary',
-    'avg_stn_pres_pst10mts_2_data_flag',
-    'avg_sea_sfc_temp_pst10mts',
-    'avg_sea_sfc_temp_pst10mts_qa_summary',
-    'avg_sea_sfc_temp_pst10mts_data_flag',
-    'avg_sea_sfc_temp_pst10mts_1',
-    'avg_sea_sfc_temp_pst10mts_1_qa_summary',
-    'avg_sea_sfc_temp_pst10mts_1_data_flag',
-    'avg_wnd_spd_pst10mts',
-    'avg_wnd_spd_pst10mts_qa_summary',
-    'avg_wnd_spd_pst10mts_data_flag',
-    'avg_wnd_spd_pst10mts_1',
-    'avg_wnd_spd_pst10mts_1_qa_summary',
-    'avg_wnd_spd_pst10mts_1_data_flag',
-    'avg_wnd_spd_pst10mts_2',
-    'avg_wnd_spd_pst10mts_2_qa_summary',
-    'avg_wnd_spd_pst10mts_2_data_flag',
-    'avg_wnd_dir_pst10mts',
-    'avg_wnd_dir_pst10mts_qa_summary',
-    'avg_wnd_dir_pst10mts_data_flag',
-    'avg_wnd_dir_pst10mts_1',
-    'avg_wnd_dir_pst10mts_1_qa_summary',
-    'avg_wnd_dir_pst10mts_1_data_flag',
-    'avg_wnd_dir_pst10mts_2',
-    'avg_wnd_dir_pst10mts_2_qa_summary',
-    'avg_wnd_dir_pst10mts_2_data_flag',
-    'max_avg_wnd_spd_pst10mts',
-    'max_avg_wnd_spd_pst10mts_qa_summary',
-    'max_avg_wnd_spd_pst10mts_data_flag',
-    'max_avg_wnd_spd_pst10mts_1',
-    'max_avg_wnd_spd_pst10mts_1_qa_summary',
-    'max_avg_wnd_spd_pst10mts_1_data_flag',
-    'max_avg_wnd_spd_pst10mts_2',
-    'max_avg_wnd_spd_pst10mts_2_qa_summary',
-    'max_avg_wnd_spd_pst10mts_2_data_flag',
-    'wnd_snsr_vert_disp',
-    'wnd_snsr_vert_disp_qa_summary',
-    'wnd_snsr_vert_disp_data_flag',
-    'wnd_snsr_vert_disp_1',
-    'wnd_snsr_vert_disp_1_qa_summary',
-    'wnd_snsr_vert_disp_1_data_flag',
-    'wnd_snsr_vert_disp_2',
-    'wnd_snsr_vert_disp_2_qa_summary',
-    'wnd_snsr_vert_disp_2_data_flag',
-    'pk_wave_pd_pst20mts',
-    'pk_wave_pd_pst20mts_qa_summary',
-    'pk_wave_pd_pst20mts_data_flag',
-    'pk_wave_pd_pst20mts_1',
-    'pk_wave_pd_pst20mts_1_qa_summary',
-    'pk_wave_pd_pst20mts_1_data_flag',
-    'pk_wave_hgt_pst20mts',
-    'pk_wave_hgt_pst20mts_qa_summary',
-    'pk_wave_hgt_pst20mts_data_flag',
-    'pk_wave_hgt_pst20mts_1',
-    'pk_wave_hgt_pst20mts_1_qa_summary',
-    'pk_wave_hgt_pst20mts_1_data_flag',
-    'sig_wave_pd_pst20mts',
-    'sig_wave_pd_pst20mts_qa_summary',
-    'sig_wave_pd_pst20mts_data_flag',
-    'sig_wave_pd_pst20mts_1',
-    'sig_wave_pd_pst20mts_1_qa_summary',
-    'sig_wave_pd_pst20mts_1_data_flag',
-    'sig_wave_hgt_pst20mts',
-    'sig_wave_hgt_pst20mts_qa_summary',
-    'sig_wave_hgt_pst20mts_data_flag',
-    'sig_wave_hgt_pst20mts_1',
-    'sig_wave_hgt_pst20mts_1_qa_summary',
-    'sig_wave_hgt_pst20mts_1_data_flag',
-    'avg_wave_pd_pst20mts',
-    'avg_wave_pd_pst20mts_qa_summary',
-    'avg_wave_pd_pst20mts_data_flag',
-    'avg_wave_pd_pst20mts_1',
-    'avg_wave_pd_pst20mts_1_qa_summary',
-    'avg_wave_pd_pst20mts_1_data_flag',
-    'avg_wave_hgt_pst20mts',
-    'avg_wave_hgt_pst20mts_qa_summary',
-    'avg_wave_hgt_pst20mts_data_flag',
-    'avg_wave_hgt_pst20mts_1',
-    'avg_wave_hgt_pst20mts_1_qa_summary',
-    'avg_wave_hgt_pst20mts_1_data_flag',
-    'avg_max_wave_pd_pst20mts',
-    'avg_max_wave_pd_pst20mts_qa_summary',
-    'avg_max_wave_pd_pst20mts_data_flag',
-    'avg_max_wave_pd_pst20mts_1',
-    'avg_max_wave_pd_pst20mts_1_qa_summary',
-    'avg_max_wave_pd_pst20mts_1_data_flag',
-    'avg_max_wave_hgt_pst20mts',
-    'avg_max_wave_hgt_pst20mts_qa_summary',
-    'avg_max_wave_hgt_pst20mts_data_flag',
-    'avg_max_wave_hgt_pst20mts_1',
-    'avg_max_wave_hgt_pst20mts_1_qa_summary',
-    'avg_max_wave_hgt_pst20mts_1_data_flag',
-    'avg_mslp_pst10mts',
-    'avg_mslp_pst10mts_qa_summary',
-    'avg_mslp_pst10mts_data_flag',
-    'avg_mslp_pst10mts_1',
-    'avg_mslp_pst10mts_1_qa_summary',
-    'avg_mslp_pst10mts_1_data_flag',
-    'avg_wtr_lvl_snsr_volt_pst10mts',
-    'avg_wtr_lvl_snsr_volt_pst10mts_qa_summary',
-    'avg_wtr_lvl_snsr_volt_pst10mts_data_flag',
-    'avg_wtr_lvl_snsr_volt_pst10mts_1',
-    'avg_wtr_lvl_snsr_volt_pst10mts_1_qa_summary',
-    'avg_wtr_lvl_snsr_volt_pst10mts_1_data_flag',
-    'pres_tend_amt_pst3hrs',
-    'pres_tend_amt_pst3hrs_qa_summary',
-    'pres_tend_amt_pst3hrs_data_flag',
-    'pres_tend_amt_pst3hrs_1',
-    'pres_tend_amt_pst3hrs_1_qa_summary',
-    'pres_tend_amt_pst3hrs_1_data_flag',
-    'pres_tend_char_pst3hrs',
-    'pres_tend_char_pst3hrs_qa_summary',
-    'pres_tend_char_pst3hrs_data_flag',
-]
+import configparser
+import logging
+
+logging.basicConfig(filename='ingest_to_csv.log', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read('msc_ingest/ingest_to_csv.conf')
+
+buoy_filename = config['DEFAULT']['buoy_list']
+output_dir = config['DEFAULT']['output_dir']
+column_headers = config['DEFAULT']['column_headers'].split(',')
 
 # file & date/time formats
-dt_format = '%Y%m%d'
-file_name_format = '%s_%s.csv'  # station id and date
+dt_format = config['DEFAULT']['datetime_format']
+file_name_format = config['DEFAULT']['filename_format'] # station id and date
+filename = None
+full_path = None
 
 def insert_into_csv(line):
-    'inserts dictionary into csv file'
-    # sampling_time = date for file name output
+    '''
+    inserts dictionary into csv file
+    '''
+    global filename, full_path
     
+    sampling_time = datetime.strptime(line['sampling_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
-    return 
+    filename = file_name_format % (line['wmo_synop_id'], sampling_time.strftime(dt_format))
+    full_path = '%s%s' % (output_dir, filename)
+
+    if os.path.isfile(full_path):
+        buoy_data = pd.read_csv(full_path)
+        buoy_data.append(line)
+    else:
+        buoy_data = pd.DataFrame(data=line, index=[0], columns=column_headers)
+
+    return buoy_data
 
 
 def remove_and_log_uninsertable_keys(line):
@@ -213,7 +72,10 @@ def read_buoys_list(filename):
 
 
 def ingest_buoy_xml_file(filename):
-    'Insert buoy data in XML format into a database'
+    '''
+    Insert buoy data in XML format into a Pandas DataFrame to be written out to
+    a CSV file
+    '''
 
     this_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -239,10 +101,11 @@ def ingest_buoy_xml_file(filename):
 @click.argument('filename', type=click.Path(exists=True))
 def main(filename):
 
-    res = ingest_buoy_xml_file(filename)
+    buoy_data = ingest_buoy_xml_file(filename)
 
-    if res:
-        print(f'{res.rowcount} rows update or inserted in directory ')
+    if not buoy_data.empty:
+        buoy_data.to_csv(path_or_buf=full_path)
+        print(f'{buoy_data.size} rows update or inserted in directory ')
 
 
 if __name__ == '__main__':
