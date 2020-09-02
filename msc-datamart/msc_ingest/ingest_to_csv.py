2b613e20cdab193c0daceea6371106fa97c6ff6c
CrazyJoeMalloy <grimmy.the.grim.reaper@gmail.com>
2020-09-02 13:46:02 -0230

    Updated paths to external files in configuration, added bulk processing option
     -dd_swob_marine.conf: updated path to plugin due to file not found complaints when subscribing, issue could be with user
    
     - ingest_to_csv.conf: made buoy list path absolute, added index field to configuration, used by Pandas to know what field should be the index
    
     - ingest_to_csv.py: added index_field options, added function to convert line data so Pandas will recognize the data as a row and not a series (values are arrays of 1 element), added bulk processing option so file path can be a directory and it will process all files in that directory (non-recursive),

--- msc-datamart/msc_ingest/ingest_to_csv.py
+++ msc-datamart/msc_ingest/ingest_to_csv.py
@@ -25,6 +25,7 @@ config.read('msc_ingest/ingest_to_csv.conf')
 buoy_filename = config['DEFAULT']['buoy_list']
 output_dir = config['DEFAULT']['output_dir']
 column_headers = config['DEFAULT']['column_headers'].split(',')
+index_field = config['DEFAULT']['index_field']
 
 # file & date/time formats
 dt_format = config['DEFAULT']['datetime_format']
@@ -43,13 +44,32 @@ def insert_into_csv(line):
     filename = file_name_format % (line['wmo_synop_id'], sampling_time.strftime(dt_format))
     full_path = '%s%s' % (output_dir, filename)
 
+    
+    converted_line = prepare_line(line)
+    
+    line_df = pd.DataFrame.from_dict(converted_line)
+    line_df.set_index(index_field, inplace=True)
+
     if os.path.isfile(full_path):
-        buoy_data = pd.read_csv(full_path)
-        buoy_data.append(line)
+        existing_data = pd.read_csv(full_path)
+        existing_data.set_index(index_field, inplace=True)
+        
+        buoy_data = existing_data.append(line_df)
     else:
-        buoy_data = pd.DataFrame(data=line, index=[0], columns=column_headers)
+        buoy_data = line_df
 
     return buoy_data
+
+def prepare_line(line):
+    '''
+    Converts line data to a form Pandas will recognize as a row of data rather than a series
+    '''
+    converted_line = {}
+
+    for key, value in line.items():
+        converted_line[key] = [value]
+
+    return converted_line
 
 
 def remove_and_log_uninsertable_keys(line):
@@ -76,37 +96,60 @@ def ingest_buoy_xml_file(filename):
     Insert buoy data in XML format into a Pandas DataFrame to be written out to
     a CSV file
     '''
-
     this_dir = os.path.dirname(os.path.realpath(__file__))
 
-    buoys_list = read_buoys_list(this_dir + '/' + buoy_filename)
+    if os.path.isfile(buoy_filename):
+        buoys_list = read_buoys_list(buoy_filename)
 
-    # get a flat dictionary from the source XML
-    buoy_record = buoy_xml_to_json(filename)
+    elif os.path.isfile(this_dir + '/' + buoy_filename):
+        logger.warning('Cannot find file as an absolute path: %s\nAttempting to locate as a relative' % (buoy_filename))
+        buoys_list = read_buoys_list(this_dir + '/' + buoy_filename)
 
-    if buoy_record['wmo_synop_id'] not in buoys_list:
-        print(
-            f"Buoy {buoy_record['wmo_synop_id']} not in: {' '.join(buoys_list)}  ")
-        return
+    else:
+        logger.error('Cannot find buoy file at path: %s!' % (buoy_filename))
 
-    # remove and print extra fields that our table doesn't have (yet)
-    # probably not necessary if we keep an eye on the mailing lists
-    remove_and_log_uninsertable_keys(buoy_record)
+    if buoys_list:
+        # get a flat dictionary from the source XML
+        buoy_record = buoy_xml_to_json(filename)
 
-    # insert the record
-    return insert_into_csv(buoy_record)
+        if buoy_record['wmo_synop_id'] not in buoys_list:
+            print(
+                f"Buoy {buoy_record['wmo_synop_id']} not in: {' '.join(buoys_list)}  ")
+            return pd.DataFrame()
 
+        # remove and print extra fields that our table doesn't have (yet)
+        # probably not necessary if we keep an eye on the mailing lists
+        remove_and_log_uninsertable_keys(buoy_record)
 
-@click.command()
-@click.argument('filename', type=click.Path(exists=True))
-def main(filename):
+        # insert the record
+        return insert_into_csv(buoy_record)
+    else:
+        return pd.DataFrame()
 
+def process_file(filename):
     buoy_data = ingest_buoy_xml_file(filename)
 
     if not buoy_data.empty:
         buoy_data.to_csv(path_or_buf=full_path)
         print(f'{buoy_data.size} rows update or inserted in directory ')
 
+
+@click.command()
+@click.option('--bulk', is_flag=True)
+@click.argument('source_path', type=click.Path(exists=True))
+def main(source_path, bulk):
+    logger.debug('Source Path: %s' % (source_path))
+    logger.debug('Bulk Flag: %s' % (bulk))
+
+    if not bulk:
+        process_file(source_path)
+    else:
+        source_directory = os.fsencode(source_path)
+            
+        for file in os.listdir(source_directory):
+            swob_xml = os.path.join(source_path, os.fsdecode(file))
+            process_file(swob_xml)
+            
 
 if __name__ == '__main__':
     main()
