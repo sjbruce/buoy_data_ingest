e2d29f7dee8af79757856e5c56e4971b2ab757e2
Scott Bruce <scott.bruce@mi.mun.ca>
2020-09-15 15:05:29 -0230

    Rough version of csv_dupe_test.py
    Useful at this point for checking a group of CSV files for the existence of duplicate records

--- msc-datamart/msc_ingest/csv_dupe_test.py
+++ msc-datamart/msc_ingest/csv_dupe_test.py
@@ -15,38 +15,60 @@ import click
 #   - do that later and base it off the command line to be self documenting
 # - 
 
-def load_files(selected_files):
+def load_files(selected_files, ignore_list):
     """
     Accepts a list of files and a list of columns to check for duplicates
     By default, all columns are checked for count, 
     list of duplicate values per column are optional 
     as is output to file (don't do that here)
     """
-    print(selected_files)
+    print("Loadings Selected Files...")
 
     df_prime = pd.DataFrame()
 
     for file_path in selected_files:
-        print(file_path)
+        # Check files against ignore list and add if not contained therein
+        print("Adding: %s ..." % (file_path))
         df_new_file = pd.read_csv(file_path)
-        # print(df_new_file.head())
+        print("# of File Records: %s" % (len(df_new_file.index)))
         df_prime = df_prime.append(df_new_file)
 
-    print(df_prime.head())
-    print(df_prime.tail())
+    print("Total records from %d selected files: %d" % (len(selected_files), len(df_prime.index)))
     return df_prime
 
-def find_duplicates(raw_data_frame, check_columns, display_type, ignore_list):
-    print(raw_data_frame, check_columns, display_type, ignore_list)
-    return
+def find_duplicates(raw_data_frame, check_columns):
+    # print("Raw Data Frame: %s, Check Columns: %s, Display Type: %s, Ignore List: %s" % (raw_data_frame, check_columns, display_type, ignore_list))
 
-def output(duplicates, sink, destination_path, destination_format):
+    if check_columns:
+        column_list = check_columns.split(',')
+        results = raw_data_frame.duplicated(subset=column_list)
+    else:
+        results = raw_data_frame.duplicated()
+
+    # print("Find Duplicates:")
+    # print("Results: \n%s" % (results))
+    return results
+
+def output(duplicates, display_type, sink, destination_path, destination_format):
     """ 
     All final displays go through here 
     default is assumed to be print to stdout 
     file output is optional as is format
     """
-    print(duplicates, sink, destination_path, destination_format)
+    duplicates_found = len(duplicates.index)
+
+    if sink == 'stdout':
+        print("Duplicates Found: %d" % (duplicates_found))
+
+        if duplicates_found:
+            print("Displaying Top 25 Results:")
+            print(duplicates.head(25))
+        else:
+            print("No duplicate records detected based on criteria.")
+
+    elif sink == 'file':
+        pass
+
     return
 
 
@@ -60,14 +82,19 @@ def get_path(source_path, regex_string):
     Use glob module
     """
     selected_files = glob.glob(source_path)
-    print(source_path, regex_string)
-    print(selected_files)
+    print("Source Path: %s, Reg Ex: %s" % (source_path, regex_string))
+    print("Files Found: %d" % (len(selected_files)))
+
+    if regex_string:
+        # apply filter to list of files
+        pass
+
     return selected_files
 
 @click.command()
 @click.argument('source_path', required=True, type=click.Path())
 @click.option('--regex_string', default=None, help='')
-@click.option('--check_columns', default='*', help='')
+@click.option('--check_columns', default=None, help='')
 @click.option('--display_type', default='COUNT', help='')
 @click.option('--ignore_list', default=None, help='')
 @click.option('--sink', default='stdout', help='')
@@ -75,11 +102,9 @@ def get_path(source_path, regex_string):
 @click.option('--destination_format', default=None, help='')
 def main(source_path, regex_string, check_columns, display_type, ignore_list, sink, destination_path, destination_format):
     selected_files = get_path(source_path, regex_string)
-    raw_data_frame = load_files(selected_files)
-    duplicates = find_duplicates(raw_data_frame, check_columns, display_type, ignore_list)
-    output_result = output(duplicates, sink, destination_path, destination_format)
-    pass
-
+    raw_data_frame = load_files(selected_files, ignore_list)
+    duplicates = find_duplicates(raw_data_frame, check_columns)
+    output(raw_data_frame[duplicates], display_type, sink, destination_path, destination_format)
 
 if __name__ == '__main__':
     main()
